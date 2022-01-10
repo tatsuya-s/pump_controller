8887669110f08d8b679abc17576f3b11ee1fb6b7
Tatsuya Sakuma <sakuma.tatsuya.sn1@is.naist.jp>
2022-01-10 13:25:35 +0900

    Use arduino to control pump

--- include/pump_controller_node.hpp
+++ include/pump_controller_node.hpp
@@ -7,12 +7,12 @@
 #include "pump_controller/ControlPump.h"
 #include "pump_controller/ControlPumpAction.h"
 
-class PumpController 
+class PumpController
 {
     public:
         PumpController();
         ~PumpController();
-        bool controlPump(pump_controller::ControlPump::Request &req, 
+        bool controlPump(pump_controller::ControlPump::Request &req,
                          pump_controller::ControlPump::Response &res);
         void controlPump(const pump_controller::ControlPumpGoal::ConstPtr &goal);
         void clear();
@@ -22,14 +22,11 @@ class PumpController
         actionlib::SimpleActionServer<pump_controller::ControlPumpAction> pump_action_server;
         ros::ServiceServer set_port_srv;
         int pi;
-        int ena_pin; // 1=ON, 0=OFF
-        int dir_pin; // 1=CW, 0=CCW
-        int pwm_pin;
-        int pwm_duty;
         double step_angle;
         std::string rasp_addr;
         std::string rasp_port;
-        bool use_const_vel;
+        std::string serial_tty;
+        int handle;
 };
 
 #endif // PUMP_CONTROLLER_NODE_HPP
--- src/pump_controller_node.cpp
+++ src/pump_controller_node.cpp
@@ -1,28 +1,20 @@
 #include "pump_controller_node.hpp"
 
 PumpController::PumpController() :
-    private_nh("~"), 
-    pump_action_server(nh, "pump_action", boost::bind(&PumpController::controlPump, this, _1), false), 
-    ena_pin(4), 
-    dir_pin(17), 
-    pwm_pin(18), 
-    pwm_duty(500000), // 50%
-    step_angle(1.8), 
-    use_const_vel(true), 
-    rasp_addr(""), 
-    rasp_port("")
+    private_nh("~"),
+    pump_action_server(nh, "pump_action", boost::bind(&PumpController::controlPump, this, _1), false),
+    step_angle(1.8),
+    rasp_addr(""),
+    rasp_port(""),
+    serial_tty("/dev/ttyUSB0")
 {
     this->pump_action_server.start();
     this->set_port_srv = this->nh.advertiseService("control_pump", &PumpController::controlPump, this);
 
-    this->private_nh.getParam("ena_pin", this->ena_pin);
-    this->private_nh.getParam("dir_pin", this->dir_pin);
-    this->private_nh.getParam("pwm_pin", this->pwm_pin);
-    this->private_nh.getParam("pwm_duty", this->pwm_duty);
     this->private_nh.getParam("step_angle", this->step_angle);
-    this->private_nh.getParam("use_const_vel", this->use_const_vel);
     this->private_nh.getParam("rasp_addr", this->rasp_addr);
     this->private_nh.getParam("rasp_port", this->rasp_port);
+    this->private_nh.getParam("serial_tty", this->serial_tty);
 
     char* char_addr = this->rasp_addr.length() > 0 ? const_cast<char*>(this->rasp_addr.c_str()) : NULL;
     char* char_port = this->rasp_port.length() > 0 ? const_cast<char*>(this->rasp_port.c_str()) : NULL;
@@ -34,42 +26,38 @@ PumpController::PumpController() :
         ROS_ERROR("GPIO error");
         ros::shutdown();
     }
-    else 
+
+    char* char_tty = this->serial_tty.length() > 0 ? const_cast<char*>(this->serial_tty.c_str()) : NULL;
+    this->handle = serial_open(this->pi, char_tty, 9600, 0);
+    if (this->handle < 0)
     {
-        ROS_INFO("Started pump action server");
+        ROS_ERROR("Serial error");
+        ros::shutdown();
     }
 
-    set_mode(this->pi, this->ena_pin, PI_OUTPUT);
-    set_mode(this->pi, this->dir_pin, PI_OUTPUT);
-    set_mode(this->pi, this->pwm_pin, PI_OUTPUT);
-    gpio_write(this->pi, this->ena_pin, PI_OFF);
-    hardware_PWM(this->pi, this->pwm_pin, 0, 0);
+    ROS_INFO("Started pump action server");
 }
 
-PumpController::~PumpController() 
+PumpController::~PumpController()
 {
 }
 
-bool PumpController::controlPump(pump_controller::ControlPump::Request &req, 
+bool PumpController::controlPump(pump_controller::ControlPump::Request &req,
                                  pump_controller::ControlPump::Response &res)
 {
-    try 
+    try
     {
-        gpio_write(this->pi, this->ena_pin, PI_ON);
-        gpio_write(this->pi, this->dir_pin, req.direction);
+        const int value = static_cast<int>(std::round(req.frequency * req.time)) * (req.direction ? 1 : -1);
+        std::string data = std::to_string(value) + "\n";
+        serial_write(this->pi, this->handle, const_cast<char*>(data.c_str()), data.size());
 
         ros::Time start_time = ros::Time::now();
-        hardware_PWM(this->pi, this->pwm_pin, req.frequency, this->pwm_duty);
-
         ros::Duration(req.time).sleep();
-
-        gpio_write(this->pi, this->ena_pin, PI_OFF);
-        hardware_PWM(this->pi, this->pwm_pin, 0, 0);
         ros::Duration duration = ros::Time::now() - start_time;
 
         res.success = true;
         res.time = duration.toSec();
-        res.revolution = static_cast<int>(this->step_angle / 360 * req.frequency * 60 * duration.toSec());
+        res.revolution = this->step_angle / 360 * value;
     }
     catch(...)
     {
@@ -93,11 +81,10 @@ void PumpController::controlPump(const pump_controller::ControlPumpGoal::ConstPt
     ros::Rate rate(1000);
     ros::Time start_time = ros::Time::now();
     ros::Duration duration;
-    const double wake_up_time = 0.5;
-    const double fall_asleep_time = goal->time - wake_up_time;
-    double frequency = 0;
 
-    while (ros::ok()) 
+    const int total_value = static_cast<int>(std::round(goal->frequency * goal->time)) * (goal->direction ? 1 : -1);
+
+    while (ros::ok())
     {
         if (this->pump_action_server.isPreemptRequested())
         {
@@ -108,54 +95,31 @@ void PumpController::controlPump(const pump_controller::ControlPumpGoal::ConstPt
 
         duration = ros::Time::now() - start_time;
 
-        if (use_const_vel) 
+        if (is_first)
         {
-            if (is_first) 
-            {
-                gpio_write(this->pi, this->ena_pin, PI_ON);
-                gpio_write(this->pi, this->dir_pin, goal->direction);
-                hardware_PWM(this->pi, this->pwm_pin, goal->frequency, this->pwm_duty);
-                is_first = false;
-            }
-        }
-        else 
-        {
-            if (wake_up_time > duration.toSec()) 
-            {
-                frequency = goal->frequency * (0.5 - 0.5 * std::cos(duration.toSec() / wake_up_time * M_PI));
-                gpio_write(this->pi, this->ena_pin, PI_ON);
-                gpio_write(this->pi, this->dir_pin, goal->direction);
-                hardware_PWM(this->pi, this->pwm_pin, frequency, this->pwm_duty);
-            }
-            else if (duration.toSec() > fall_asleep_time) 
-            {
-                frequency = goal->frequency * (0.5 + 0.5 * std::cos((duration.toSec() - fall_asleep_time) / wake_up_time * M_PI));
-                gpio_write(this->pi, this->ena_pin, PI_ON);
-                gpio_write(this->pi, this->dir_pin, goal->direction);
-                hardware_PWM(this->pi, this->pwm_pin, frequency, this->pwm_duty);
-            }
+            std::string data = std::to_string(total_value) + "\n";
+            serial_write(this->pi, this->handle, const_cast<char*>(data.c_str()), data.size());
+            is_first = false;
         }
 
-        if (duration.toSec() >= goal->time) 
+        if (duration.toSec() >= goal->time)
         {
             success = true;
             break;
         }
 
         feedback.time = duration.toSec();
-        feedback.revolution = static_cast<int>(this->step_angle / 360 * goal->frequency * 60 * duration.toSec());
+        const int curr_value = static_cast<int>(std::round(goal->frequency * feedback.time)) * (goal->direction ? 1 : -1);
+        feedback.revolution = this->step_angle / 360 * curr_value;
         this->pump_action_server.publishFeedback(feedback);
 
         rate.sleep();
     }
 
-    gpio_write(this->pi, this->ena_pin, PI_OFF);
-    hardware_PWM(this->pi, this->pwm_pin, 0, 0);
-
     if (success)
     {
         result.time = duration.toSec();
-        result.revolution = static_cast<int>(this->step_angle / 360 * goal->frequency * 60 * duration.toSec());
+        result.revolution = this->step_angle / 360 * total_value;
 
         this->pump_action_server.setSucceeded(result);
     }
@@ -163,13 +127,11 @@ void PumpController::controlPump(const pump_controller::ControlPumpGoal::ConstPt
 
 void PumpController::clear()
 {
-    set_mode(this->pi, this->ena_pin, PI_INPUT);
-    set_mode(this->pi, this->dir_pin, PI_INPUT);
-    set_mode(this->pi, this->pwm_pin, PI_INPUT);
+    serial_close(this->pi, this->handle);
     pigpio_stop(this->pi);
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "pump_controller_node");
 
